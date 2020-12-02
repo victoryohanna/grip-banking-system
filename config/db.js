2eb2b4632e7b60249ecfc4c0479210869325485c
victoryohanna <victoryohanna@gmail.com>
2020-12-02 12:58:14 +0100

    fix errors

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const { success, error } = require("consola");
 
 require("dotenv").config();
-//const conn = process.env.CONN_STRING;    
-const conn = "mongodb+srv://victoryohanna:tanams5562@cluster0-pizvr.mongodb.net/bankdb"
+const conn = process.env.CONN_STRING;    
+
 const dbInit = async () => {
   await mongoose
     .connect(conn, {
